7562dd4bc12f740059c71a22a48cd571bbf42360
TDanishMahajan <danishm@tejasnetworks.com>
2024-04-23 16:38:46 +0530

    added ui to admin page

---
+++ client/src/components/Header/Admin/Hostel.tsx
@@ -0,0 +1,146 @@
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const DropdownUser = () => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const trigger = useRef<any>(null);
+    const dropdown = useRef<any>(null);
+
+    // close on click outside
+    useEffect(() => {
+        const clickHandler = ({ target }: MouseEvent) => {
+            if (!dropdown.current) return;
+            if (
+                !dropdownOpen ||
+                dropdown.current.contains(target) ||
+                trigger.current.contains(target)
+            )
+                return;
+            setDropdownOpen(false);
+        };
+        document.addEventListener('click', clickHandler);
+        return () => document.removeEventListener('click', clickHandler);
+    });
+
+    // close if the esc key is pressed
+    useEffect(() => {
+        const keyHandler = ({ keyCode }: KeyboardEvent) => {
+            if (!dropdownOpen || keyCode !== 27) return;
+            setDropdownOpen(false);
+        };
+        document.addEventListener('keydown', keyHandler);
+        return () => document.removeEventListener('keydown', keyHandler);
+    });
+
+    return (
+        <div className="relative">
+            <Link
+                ref={trigger}
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+                className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-pink-700 dark:hover:bg-blue-950`}
+                to="#"
+            >
+                <svg
+                    className="fill-current"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M1.43425 7.5093H2.278C2.44675 7.5093 2.55925 7.3968 2.58737 7.31243L2.98112 6.32805H5.90612L6.27175 7.31243C6.328 7.48118 6.46862 7.5093 6.58112 7.5093H7.453C7.76237 7.48118 7.87487 7.25618 7.76237 7.03118L5.428 1.4343C5.37175 1.26555 5.3155 1.23743 5.14675 1.23743H3.88112C3.76862 1.23743 3.59987 1.29368 3.57175 1.4343L1.153 7.08743C1.0405 7.2843 1.20925 7.5093 1.43425 7.5093ZM4.47175 2.98118L5.3155 5.17493H3.59987L4.47175 2.98118Z"
+                        fill=""
+                    />
+                    <path
+                        d="M10.1249 2.5031H16.8749C17.2124 2.5031 17.5218 2.22185 17.5218 1.85623C17.5218 1.4906 17.2405 1.20935 16.8749 1.20935H10.1249C9.7874 1.20935 9.47803 1.4906 9.47803 1.85623C9.47803 2.22185 9.75928 2.5031 10.1249 2.5031Z"
+                        fill=""
+                    />
+                    <path
+                        d="M16.8749 6.21558H10.1249C9.7874 6.21558 9.47803 6.49683 9.47803 6.86245C9.47803 7.22808 9.75928 7.50933 10.1249 7.50933H16.8749C17.2124 7.50933 17.5218 7.22808 17.5218 6.86245C17.5218 6.49683 17.2124 6.21558 16.8749 6.21558Z"
+                        fill=""
+                    />
+                    <path
+                        d="M16.875 11.1656H1.77187C1.43438 11.1656 1.125 11.4469 1.125 11.8125C1.125 12.1781 1.40625 12.4594 1.77187 12.4594H16.875C17.2125 12.4594 17.5219 12.1781 17.5219 11.8125C17.5219 11.4469 17.2125 11.1656 16.875 11.1656Z"
+                        fill=""
+                    />
+                    <path
+                        d="M16.875 16.1156H1.77187C1.43438 16.1156 1.125 16.3969 1.125 16.7625C1.125 17.1281 1.40625 17.4094 1.77187 17.4094H16.875C17.2125 17.4094 17.5219 17.1281 17.5219 16.7625C17.5219 16.3969 17.2125 16.1156 16.875 16.1156Z"
+                        fill="white"
+                    />
+                </svg>
+                Hostel
+            </Link>
+
+            {/* <!-- Dropdown Start --> */}
+            <div
+                ref={dropdown}
+                onFocus={() => setDropdownOpen(true)}
+                onBlur={() => setDropdownOpen(false)}
+                className={`absolute right-0 mt-4 flex w-62.5 flex-col rounded-sm border border-stroke bg-blue-950 shadow-default dark:border-strokedark dark:bg-pink-700 ${dropdownOpen === true ? 'block' : 'hidden'
+                    }`}
+            >
+                <ul className="hidden sm:block">
+                    <li>
+                        <Link
+                            to="/getHostelDetails"
+                            className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-pink-700 dark:hover:bg-blue-950
+                              `}
+                        >
+                            Hostel Details
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/createHostel"
+                            className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-pink-700 dark:hover:bg-blue-950
+                              `}
+                        >
+                            Create Hostel
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                           to="/deleteHostel"
+                            className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-pink-700 dark:hover:bg-blue-950
+                              `}
+                        >
+                            Delete Hostel
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/showAllWardenDetails"
+                            className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-pink-700 dark:hover:bg-blue-950
+                              `}
+                        >
+                            Wardens Details
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                           to="/createWarden"
+                            className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-pink-700 dark:hover:bg-blue-950
+                              `}
+                        >
+                            Create Warden
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/deleteWarden"
+                            className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-pink-700 dark:hover:bg-blue-950
+                              `}
+                        >
+                            Delete Warden
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            {/* <!-- Dropdown End --> */}
+        </div>
+    );
+};
+
+export default DropdownUser;
