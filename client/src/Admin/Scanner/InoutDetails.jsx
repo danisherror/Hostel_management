3bc8e34f9ca20e2f15dd8722710d6ab0d1ad091e
TDanishMahajan <danishm@tejasnetworks.com>
2024-04-30 20:21:53 +0530

    all functionality forking fine

--- client/src/Admin/Scanner/InoutDetails.jsx
+++ client/src/Admin/Scanner/InoutDetails.jsx
@@ -57,7 +57,7 @@ const TableThree = () => {
           }
         },
         legend: {
-          show: false
+            position: 'bottom'
         },
         tooltip: {
           theme: 'dark'
